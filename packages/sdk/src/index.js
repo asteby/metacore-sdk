401dc222789eec2cc1c4b2a60ef5d80d390447cd
akromicc <ytdracogames@gmail.com>
2026-05-11 18:44:52 -0500

    chore(sdk): remove .js cruft alongside .ts in src/
    Pre-dist migration artifacts: tracked .js files duplicated .ts sources
    in packages/sdk/src/ and confused vitest module resolution. The dist/
    output handles publishing; src/ should only carry .ts.

--- packages/sdk/src/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./types.js";
-export * from "./client.js";
-export * from "./registry.js";
-export * from "./plugin.js";
-export * from "./federation.js";
-export * from "./bootstrap.js";
-export * from "./action-registry.js";
